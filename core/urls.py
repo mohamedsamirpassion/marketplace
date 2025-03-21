e8e8a583c3c318db56760c264f0782c9b3ff01a7
mohamedsamirpassion <muhammadsamir777@gmail.com>
2025-03-21 17:38:18 -0400

    Added home page content and user registration

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, submit_ad
+from .views import home, submit_ad, signup
 
 urlpatterns = [
     path('', home, name='home'),
     path('ads/create/', submit_ad, name='submit_ad'),
+    path('signup/', signup, name='signup'),  # Add this line
 ]
--- core/views.py
+++ core/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Car
 from .forms import CarForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     cars = Car.objects.filter(status='approved')
     return render(request, 'core/home.html', {'cars': cars})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'core/signup.html', {'form': form})
+
 
 @login_required
 def submit_ad(request):
